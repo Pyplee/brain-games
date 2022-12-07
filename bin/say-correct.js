e850141ca689c3b38180e22fe965ac324855db69
Pyplee <jobmyilmy@gmail.com>
2022-12-07 17:12:16 +0300

    update var in say=correct

--- bin/say-correct.js
+++ bin/say-correct.js
@@ -1,8 +1,8 @@
-const sayCorrectOrUncorrect = (boolean, answerUser, result, userName) => {
+const sayCorrectOrUncorrect = (boolean, answerUser, answerCorrect, userName) => {
   if (boolean === true) {
     console.log('Correct!');
   } else if (boolean === false) {
-    console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${result}'.`);
+    console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answerCorrect}'.`);
     console.log(`Let's try again, ${userName}!`);
   }
 };
