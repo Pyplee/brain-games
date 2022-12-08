e45a48b5380c6b6383a4870dbcd8adc2e5caaad2
Pyplee <jobmyilmy@gmail.com>
2022-12-08 14:25:56 +0300

    update - work

--- games/brain-gcd.js
+++ games/brain-gcd.js
@@ -1,35 +1,42 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import helloUser from '../src/cli.js';
 import generateNumber from '../bin/generate-numbers.js';
 import checkAnswer from '../bin/check-answer.js';
-import sayCorrectOrUncorrect from '../bin/say-correct.js'; // boolean, answerUser, result, userName
+import sayCorrectOrUncorrect from '../bin/say-correct.js'; // boolean, userAnswerToQuestion, correctAnswerToQuestion, userName
 
 const userName = helloUser();
 console.log('Find the greatest common divisor of given numbers.');
 
-const generateNumbAndDivider = () => {
-  const arrayNumb = [];
-  const tempForCheckNumb = generateNumber(1);
-  arrayNumb.push(tempForCheckNumb);
-  const tempForCheckDivider = generateNumber(0);
-  arrayNumb.push(tempForCheckDivider);
-  return arrayNumb;
+const generateNumbAndKey = () => {
+  const singleDigitNumbers = 0;
+  const twoDigitNumbers = 1;
+  const startArray = [];
+  const startNumb = generateNumber(twoDigitNumbers);
+  startArray.push(startNumb);
+  const keyNumb = generateNumber(singleDigitNumbers);
+  startArray.push(keyNumb);
+  return startArray;
 };
 
 const gcd = () => {
+  let resultGamesBoolean = true;
   for (let i = 0; i < 3; i += 1) {
-    const arrayMain = generateNumbAndDivider();
-    const firstNumb = arrayMain[0];
-    const secondNumb = arrayMain[0] * arrayMain[1];
-    const answerDivider = arrayMain[1];
-    console.log(`Question: ${firstNumb} ${secondNumb}`);
-    const answerUser = readlineSync.question('Your answer: ');
-    if (checkAnswer(answerUser, answerDivider, 1)) {
+    const arrayWithInitialNumbAndKey = generateNumbAndKey();
+    const firstNumbInTheQuestion = arrayWithInitialNumbAndKey[0];
+    const secondNumbInTheQuestion = arrayWithInitialNumbAndKey[0] * arrayWithInitialNumbAndKey[1];
+    const correctAnswerToQuestion = arrayWithInitialNumbAndKey[1];
+    console.log(`Question: ${firstNumbInTheQuestion} ${secondNumbInTheQuestion}`);
+    const userAnswerToQuestion = readlineSync.question('Your answer: ');
+    if (checkAnswer(userAnswerToQuestion, correctAnswerToQuestion, 'yes')) {
       sayCorrectOrUncorrect(true);
     } else {
-      sayCorrectOrUncorrect(false, answerUser, answerDivider, userName);
+      sayCorrectOrUncorrect(false, userAnswerToQuestion, correctAnswerToQuestion, userName);
+      resultGamesBoolean = false;
       break;
     }
+  }
+  if (resultGamesBoolean === true) {
     console.log(`Congratulations, ${userName}!`);
   }
 };
