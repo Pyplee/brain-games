e6ed5b9e668a37959cc39e287de980b426b66708
Pyplee <jobmyilmy@gmail.com>
2022-12-08 14:27:29 +0300

    refact

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -1,17 +1,21 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import helloUser from '../src/cli.js';
 import generateNumber from '../bin/generate-numbers.js';
 import checkAnswer from '../bin/check-answer.js';
-import sayCorrectOrUncorrect from '../bin/say-correct.js';
+import sayCorrectOrUncorrect from '../bin/say-correct.js'; // boolean, userAnswerToQuestion, correctAnswerToQuestion, userName
 
 const userName = helloUser();
 console.log('What number is missing in the progression?');
+
+const singleDigitNumbers = 0;
+const twoDigitNumbers = 1;
 
 const generateStartNumber = () => {
   let startNumberLocal = 0;
   const maxNumberForGenerate = 20;
   for (let j = 0; j < 11; j += 1) {
-    const startNumberTemp = generateNumber(1);
+    const startNumberTemp = generateNumber(twoDigitNumbers);
     if (startNumberTemp <= maxNumberForGenerate) {
       startNumberLocal = startNumberTemp;
       break;
@@ -21,31 +25,34 @@ const generateStartNumber = () => {
 };
 
 const generateKeyNumber = () => {
-  const maxKeyForGenerate = 8;
-  let keyNumberLocal = 0;
+  const maxKeyForGenerate = 8; // max start number-20, max key-8, repeats-10
+  // progression-[1....10] => 8*10=80 max numb if start numb - 1, if start numb 20,
+  // max 10th index - 100. Restriction - 100. (20 + 8 + 8 + 8... | 20 + 8*10 = 100)
+  let keyNumber = 0;
   for (let o = 0; o < 11; o += 1) {
-    const keyNumberTemp = generateNumber(0);
-    if (keyNumberTemp <= maxKeyForGenerate && keyNumberTemp > 0) {
-      keyNumberLocal = keyNumberTemp;
+    const keyNumberForCheck = generateNumber(singleDigitNumbers);
+    if (keyNumberForCheck <= maxKeyForGenerate && keyNumberForCheck > 0) {
+      keyNumber = keyNumberForCheck;
       break;
     }
   }
-  return keyNumberLocal;
+  return keyNumber;
 };
 
-const generateNumbHide = () => {
+const generateIndexNumbForbHide = () => {
   let hideNumbLocal = 0;
   for (let i = 0; i < 11; i += 1) {
-    const hideNumbTemp = generateNumber(0);
-    if (hideNumbTemp > 1 && hideNumbLocal < 11) {
-      hideNumbLocal = hideNumbTemp;
+    const hideNumbForCheck = generateNumber(0);
+    if (hideNumbForCheck > 1 && hideNumbLocal < 11) {
+      hideNumbLocal = hideNumbForCheck;
       break;
     }
   }
   return hideNumbLocal;
 };
 
-const progression = () => {
+const playProgression = () => {
+  let resultGamesBoolean = true;
   for (let j = 0; j < 3; j += 1) {
     const resultArray = [];
     const keyNumber = generateKeyNumber();
@@ -53,22 +60,25 @@ const progression = () => {
     resultArray.push('Question:');
     resultArray.push(startNumber);
     for (let i = 1; i < 10; i += 1) {
-      const calc = resultArray[i] + keyNumber;
-      resultArray.push(calc);
+      const calcProgression = resultArray[i] + keyNumber;
+      resultArray.push(calcProgression);
     }
-    const hideNumber = generateNumbHide();
-    const answerCorrect = resultArray[hideNumber];
-    resultArray[hideNumber] = '..';
+    const indexForHideNumb = generateIndexNumbForbHide();
+    const correctAnswerToQuestion = resultArray[indexForHideNumb];
+    resultArray[indexForHideNumb] = '..';
     console.log(...resultArray);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (checkAnswer(userAnswer, answerCorrect, 1)) {
+    const userAnswerToQuestion = readlineSync.question('Your answer: ');
+    if (checkAnswer(userAnswerToQuestion, correctAnswerToQuestion, 'yes')) {
       sayCorrectOrUncorrect(true);
     } else {
-      sayCorrectOrUncorrect(false, userAnswer, answerCorrect, userName);
+      sayCorrectOrUncorrect(false, userAnswerToQuestion, correctAnswerToQuestion, userName);
+      resultGamesBoolean = false;
       break;
     }
+  }
+  if (resultGamesBoolean === true) {
     console.log(`Congratulations, ${userName}!`);
   }
 };
 
-progression();
+playProgression();
