21e002b0ff27ff7e975da42da980abc7cf7f30f4
Pyplee <jobmyilmy@gmail.com>
2022-12-08 14:28:14 +0300

    refact

--- bin/say-correct.js
+++ bin/say-correct.js
@@ -1,8 +1,9 @@
-const sayCorrectOrUncorrect = (boolean, answerUser, answerCorrect, userName) => {
+/* eslint-disable max-len */
+const sayCorrectOrUncorrect = (boolean, userAnswerToQuestion, correctAnswerToQuestion, userName) => {
   if (boolean === true) {
     console.log('Correct!');
   } else if (boolean === false) {
-    console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answerCorrect}'.`);
+    console.log(`'${userAnswerToQuestion}' is wrong answer ;(. Correct answer was '${correctAnswerToQuestion}'.`);
     console.log(`Let's try again, ${userName}!`);
   }
 };
