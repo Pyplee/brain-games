01651582b45eb3a36d4e7c2523441f87b50f921b
pyplee <dekimiq@gmail.com>
2023-03-02 10:17:46 +0300

    fix export

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import helloUser from '../src/cli.js';
-import generateNumber from '../bin/generate-numbers.js';
-import checkAnswer from '../bin/check-answer.js';
-import sayCorrectOrUncorrect from '../bin/say-correct.js'; // boolean, userAnswerToQuestion, correctAnswerToQuestion, userName
+import helloUser from '../cli.js';
+import generateNumber from '../generate-numbers.js';
+import checkAnswer from '../check-answer.js';
+import sayCorrectOrUncorrect from '../say-correct.js'; // boolean, userAnswerToQuestion, correctAnswerToQuestion, userName
 
 const userName = helloUser();
 console.log('What is the result of the expression?');
@@ -60,4 +60,4 @@ const playCalculate = () => {
   }
 };
 
-playCalculate();
+export default playCalculate;
