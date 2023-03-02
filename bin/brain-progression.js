de5be3f22e8b5baa8057db585c47e3943cab4115
pyplee <dekimiq@gmail.com>
2023-03-02 15:12:20 +0300

    rename func

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import playProgression from '../src/games/progression.js';
+import getPlayProgression from '../src/games/progression.js';
 
-playProgression();
+getPlayProgression();
