6797aaa855d8c0b8d1cacd4fdc0ec4828ca0862e
pyplee <dekimiq@gmail.com>
2023-03-02 14:57:43 +0300

    rewrite all gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,22 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import helloUser from '../cli.js';
-import generateNumber from '../generate-numbers.js';
-import checkAnswer from '../check-answer.js';
-import sayCorrectOrUncorrect from '../say-correct.js'; // boolean, userAnswerToQuestion, correctAnswerToQuestion, userName
-
-const userName = helloUser();
-console.log('Find the greatest common divisor of given numbers.');
-
-const generateNumbAndKey = () => {
-  const twoDigitNumbers = 1;
-  const startArray = [];
-  const firstNumb = generateNumber(twoDigitNumbers);
-  startArray.push(firstNumb);
-  const secondNumb = generateNumber(twoDigitNumbers);
-  startArray.push(secondNumb);
-  return startArray;
-};
+import runEngine from '../index.js';
+import getRandomInRange from '../utils.js';
 
 const gcd = (a, b) => {
   if (!b) {
@@ -25,26 +9,17 @@ const gcd = (a, b) => {
   return gcd(b, a % b);
 };
 
-const playGCD = () => {
-  let resultGamesBoolean = true;
+const getPlayGcd = () => {
+  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const questions = [];
+  const correctAnswer = [];
   for (let i = 0; i < 3; i += 1) {
-    const arrayWithInitialNumbAndKey = generateNumbAndKey();
-    const firstNumbInTheQuestion = arrayWithInitialNumbAndKey[0];
-    const secondNumbInTheQuestion = arrayWithInitialNumbAndKey[1];
-    const correctAnswerToQuestion = gcd(firstNumbInTheQuestion, secondNumbInTheQuestion);
-    console.log(`Question: ${secondNumbInTheQuestion} ${firstNumbInTheQuestion}`);
-    const userAnswerToQuestion = readlineSync.question('Your answer: ');
-    if (checkAnswer(userAnswerToQuestion, correctAnswerToQuestion, 'yes')) {
-      sayCorrectOrUncorrect(true);
-    } else {
-      sayCorrectOrUncorrect(false, userAnswerToQuestion, correctAnswerToQuestion, userName);
-      resultGamesBoolean = false;
-      break;
-    }
+    const x = getRandomInRange(1, 50);
+    const y = getRandomInRange(1, 50);
+    questions.push(`${x} ${y}`);
+    correctAnswer.push(gcd(x, y));
   }
-  if (resultGamesBoolean === true) {
-    console.log(`Congratulations, ${userName}!`);
-  }
+  runEngine(rules, [questions, correctAnswer]);
 };
 
-export default playGCD;
+export default getPlayGcd;
