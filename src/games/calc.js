f412f887a25ef57e231c3745aac55f960d2f129a
pyplee <dekimiq@gmail.com>
2023-03-02 17:21:56 +0300

    refact code

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,30 +2,45 @@
 import getRandomInRange from '../utils.js';
 import runEngine from '../index.js';
 
+const getXY = (rate1, rate2) => {
+  const x = getRandomInRange(rate1, rate2);
+  const y = getRandomInRange(rate1, rate2);
+  return [x, y];
+};
+
+const getCalcAnswer = (x, y, operator) => {
+  let result = 0;
+  switch (operator) {
+    case '+':
+      result = x + y;
+      break;
+    case '-':
+      result = x - y;
+      break;
+    default:
+      result = x * y;
+      break;
+  }
+  return result;
+};
+
 const getPlayCalculate = () => {
   const rules = 'What is the result of the expression?';
   const questions = [];
   const correctAnswer = [];
+  const operators = ['+', '-', '*'];
+  const rateGenNumMinOperator = 0;
+  const rateGenNumMaxOperator = 2;
+  const rateGenNumMin = 0;
+  let rateGenNumMax = 90;
   for (let i = 0; i < 3; i += 1) {
-    const randomOperator = getRandomInRange(1, 3);
-    if (randomOperator === 1) { // -
-      const x = getRandomInRange(0, 50);
-      const y = getRandomInRange(0, 50);
-      questions.push(`${x} - ${y}`);
-      correctAnswer.push((x - y));
+    const operator = operators[getRandomInRange(rateGenNumMinOperator, rateGenNumMaxOperator)];
+    if (operator === '*') {
+      rateGenNumMax = 10;
     }
-    if (randomOperator === 2) { // +
-      const x = getRandomInRange(0, 50);
-      const y = getRandomInRange(0, 50);
-      questions.push(`${x} + ${y}`);
-      correctAnswer.push((x + y));
-    }
-    if (randomOperator === 3) { // *
-      const x = getRandomInRange(0, 20);
-      const y = getRandomInRange(0, 10);
-      questions.push(`${x} * ${y}`);
-      correctAnswer.push((x * y));
-    }
+    const [x, y] = getXY(rateGenNumMin, rateGenNumMax);
+    questions.push(`${x} ${operator} ${y}`);
+    correctAnswer.push(getCalcAnswer(x, y, operator));
   }
   runEngine(rules, [questions, correctAnswer]);
 };
