9bd611ed89fa3a0505d84c5cc903d04c648c30ee
pyplee <dekimiq@gmail.com>
2023-03-02 14:22:08 +0300

    all delete and write func for questons nmae

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,5 @@
 import readlineSync from 'readline-sync';
 
-const helloUser = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return userName;
-};
+const helloUser = () => readlineSync.question('May I have your name? ');
 
 export default helloUser;
